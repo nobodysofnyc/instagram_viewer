8af739fa20089454fa8ed50c1a44aaf1dbbf0f44
mikekavouras <mike@wantful.com>
2014-01-20 22:37:46 -0500

    instagramslideshow

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function doThisWhenDone(obj) {
-  for (var i = 0; i < obj._postsFilteredByTag.length; i++) {
-    var post = obj._postsFilteredByTag[i];
-    var src = post.images.standard_resolution.url;
-    var tags = JSON.stringify(post.tags);
-    var img = $('<img data-id="'+ post.id +'" data-tags="'+ tags +'" class="insta-image" src="' + src + '">');
-    $('#images').append(img);
-  }
-}
-
 function ensureHashTag($elem, e) {
   var k = e.keyCode;
   if (k === 91 || k === 37 || k == 38 || k == 39 || k == 40 || k == 9) return;
@@ -27,7 +17,7 @@ $(document).ready(function() {
     var username = $('#username').val();
 
     var success = function(obj) {
-      doThisWhenDone(obj);
+      var slideshow = new InstagramSlideshow(obj);
     }
     // new InstagramQuery(username, tag, limit, success)
     var query = new InstagramQuery(username, tag, 100, success);
